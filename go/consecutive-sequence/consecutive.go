84b56dc56391619929f6f6e9451f6e22fdea6af0
Matt Eva <matteweva@gmail.com>
2024-06-04 17:13:53 -0700

    saving longest-consecutive solution

--- go/consecutive-sequence/consecutive.go
+++ go/consecutive-sequence/consecutive.go
@@ -1,5 +1,140 @@
 package main
 
-func main(){
+// O(N) struct example - final solution
+// Ironically, the solution that just uses slices.Sort runs the fastest
+// and uses the least memory
+
+func longestConsecutive(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+    type NumStruct struct {
+        Visited bool
+    }
 	
+	set := make(map[int]*NumStruct)
+	max := 1
+
+	for _, val := range nums {
+		set[val] = &NumStruct {
+            false,
+        }
+	}
+
+	for _, num := range nums {
+        if set[num].Visited{
+            continue
+        }
+
+        count := -1
+        for current := num; set[current] != nil; current++ {
+            count++
+            set[current].Visited = true
+        }
+
+		for current :=num; set[current] != nil; current--{
+			count++
+			set[current].Visited = true
+		}
+        
+        if count > max{
+            max = count
+        }
+	}
+
+	return max
 }
+
+// MAP SOLUTION
+
+// func longestConsecutive(nums []int) int {
+// 	if len(nums) == 0 {
+// 		return 0
+// 	}
+
+// 	set := make(map[int]bool)
+// 	max := 1
+
+// 	for _, val := range nums {
+// 		set[val] = true
+// 	}
+
+// 	for _, num := range nums {
+// 		if set[num-1] {
+// 			continue
+// 		}
+
+// 		count := 0
+// 		for current := num; set[current]; current++ {
+// 			count++
+// 		}
+
+// 		if count > max {
+// 			max = count
+// 		}
+// 	}
+
+// 	return max
+// }
+
+//     return newMax
+// }
+
+// OVERLY COMPLICATED HEAP SOLUTION
+
+// func main() {
+// 	longestConsecutive([]int{100,4,200,1,3,2})
+// }
+
+// func longestConsecutive(nums []int) int {
+// 	numMap := make(map[int]bool)
+// 	numHeap := &IntHeap{}
+// 	heap.Init(numHeap)
+
+// 	for _, num := range nums{
+// 		if !numMap[num]{
+// 			numMap[num] = true
+// 			heap.Push(numHeap, num)
+// 		}
+// 	}
+
+// 	max := 0
+// 	for numHeap.Len() > 0 {
+// 		newMax := 0
+// 		for minimum := (*numHeap)[0]; ;minimum++ {
+// 			if numMap[minimum]{
+// 				newMax += 1
+// 				heap.Pop(numHeap)
+
+// 				if newMax > max{
+// 					max = newMax
+// 				}
+// 			} else {
+// 				break
+// 			}
+// 		}
+// 	}
+
+// 	return max
+// }
+
+// type IntHeap []int
+
+// func (h IntHeap) Len() int {return len(h)}
+// func (h IntHeap) Less(i, j int) bool {return h[i] < h[j]}
+// func (h IntHeap) Swap(i, j int) { h[i], h[j] = h[j], h[i]}
+
+// func (h *IntHeap) Pop() any {
+// 	old := *h
+// 	length := len(old)
+// 	leaf := old[length - 1]
+// 	*h = old[0 : length - 1]
+// 	return leaf
+// }
+
+// func (h *IntHeap) Push(x any) {
+// 	if integer, ok := x.(int); ok {
+// 		*h = append(*h, integer)
+// 	}
+// }
--- go/consecutive-sequence/consecutive_test.go
+++ go/consecutive-sequence/consecutive_test.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestLongestConsecutive(t *testing.T) {
+	case1 := []int{100, 4, 200, 1, 3, 2}
+	answer1 := 4
+	testResult1 := longestConsecutive(case1)
+	fmt.Println(testResult1)
+	if answer1 != testResult1 {
+		t.Fatal("first results did not match!")
+	}
+	case2 := []int{10,9,8,7,6,5,4,3,2,1}
+	answer2 := 10
+	testResult2 := longestConsecutive(case2)
+	if answer2 != testResult2 {
+		t.Fatal("Second results did not match!")
+	}
+}
