cc18cb547c12e26be4e7562a3336059469619a3b
Matt Eva <matteweva@gmail.com>
2024-10-29 15:54:38 -0700

    solved problem using sorted array and two pointers

--- go/three_sum/three_sum.go
+++ go/three_sum/three_sum.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+)
 
 func main() {
 
@@ -8,25 +11,54 @@ func main() {
 	// fmt.Println(threeSum([]int{0, 1, 1}))
 	// fmt.Println(threeSum([]int{0, 0, 0}))
 	// fmt.Println(threeSum([]int{-1, 0, 1}))
-    fmt.Println(threeSum([]int{-1, 0, 1, -9, 0, 9, 5, 4}))
+    // fmt.Println(threeSum([]int{-1, 0, 1, -9, 0, 9, 5, 4}))
+    // fmt.Println(threeSum([]int{0,0,0, 0}))
+    // fmt.Println(threeSum([]int{-2,0,0,2,2}))
+    fmt.Println(threeSum([]int{-4,-2,1,-5,-4,-4,4,-2,0,4,0,-2,3,1,-5,0}))
 
 }
 
 func threeSum(nums []int) [][]int {
 
+    slices.Sort(nums)
+    fmt.Println(nums)
 
 	result := make([][]int, 0)
 
     for i, num := range nums {
-        
-        numMap := make(map[int][]int)
-        for j := i + 1; j < len(nums); j++{
-            numMap[nums[j] + num] = []int{nums[j], num}
+        if (i > 0 && num == nums[i-1]){
+            continue
         }
-        for j := i + 1; j < len(nums); j++ {
-            if len(numMap[nums[j] * -1]) == 2{
-                resultArr := []int{nums[j], numMap[nums[j] * -1][0], numMap[nums[j] * -1][1]}
+
+        negativeNum := num * -1
+        j := i + 1
+        k := len(nums) - 1
+
+        incremented := false
+        decremented := false
+        for !(j >= k) {
+            num2 := nums[j]
+            num3 := nums[k]
+            if incremented && num2 == nums[j - 1]{
+                j++
+            } else if decremented && num2 == nums[k + 1]{
+                incremented = false
+                k--
+            } else if (num2 + num3) == negativeNum{
+                resultArr := []int{num, num2, num3}
                 result = append(result, resultArr)
+                j++
+                k--
+                incremented = true
+                decremented = true
+            } else if (num2 + num3) < negativeNum{
+                j++
+                incremented= true
+                decremented = false
+            } else{
+                k--
+                incremented = false
+                decremented = true
             }
         }
     }
